1e502e1f9662b4de3b013076194a59130d4f804a
m-abouruways <m.abouruways@studenti.unisa.it>
2026-01-28 21:54:11 +0100

    Refactor: Fix non-serializable Comparator bug in TreeBag

--- src/main/java/org/apache/commons/collections4/bag/TreeBag.java
+++ src/main/java/org/apache/commons/collections4/bag/TreeBag.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -147,7 +147,11 @@ public class TreeBag<E> extends AbstractMapBag<E> implements SortedBag<E>, Seria
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
-        out.writeObject(comparator());
+        if (comparator() instanceof Serializable || comparator() == null) {
+            out.writeObject(comparator());
+        } else {
+            out.writeObject(null);
+        }
         super.doWriteObject(out);
     }
 
